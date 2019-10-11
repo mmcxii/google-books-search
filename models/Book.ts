f8fd4e86df57790a85e8d61713f351e02d8e9d06
Nich Secord <nicholassecord@gmail.com>
2019-10-11 11:55:38 -0700

    fixed array typing

--- models/Book.ts
+++ models/Book.ts
@@ -3,7 +3,7 @@ import { createSchema, Type, typedModel } from 'ts-mongoose';
 // Creat Schema
 const BookSchema = createSchema({
   title: Type.string(),
-  authors: Type.array({ of: Type.string() }),
+  authors: Type.array().of(Type.string()),
   description: Type.string(),
   image: Type.string(),
   link: Type.string(),
