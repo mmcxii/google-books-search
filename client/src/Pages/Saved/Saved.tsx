fed9180faccfcf19403c9655fbcc2ab6ba22c07f
Nich Secord <nicholassecord@gmail.com>
2019-10-13 17:47:51 -0700

    blank page

---
+++ client/src/Pages/Saved/Saved.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface Props {}
+
+const Saved: React.FC<Props> = () => {
+  return (
+    <>
+      <h2>Saved Books</h2>
+    </>
+  );
+};
+
+export default Saved;
